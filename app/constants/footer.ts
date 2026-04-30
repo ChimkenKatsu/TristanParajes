c9e590dbb1f60474bd346bcfb9fd0c135145bfd1
Tristan <tristandominicparajes.202200583@gmail.com>
2026-04-30 16:50:54 +0800

    Code kay way gi buhat

--- app/constants/footer.ts
+++ app/constants/footer.ts
@@ -5,30 +5,30 @@ export const FOOTER_LINKS: FooterLink[] = [
     name: 'LinkedIn',
     hoverText: 'Connect with me',
     icon: 'icons/linkedin.svg',
-    url: 'https://www.linkedin.com/in/mohit-virli-4780b6112/',
+    url: 'https://www.linkedin.com/in/tristan-parajes-28b587271/',
   },
   {
     name: 'GitHub',
     hoverText: 'Open Sourcing',
     icon: 'icons/github.svg',
-    url: 'https://github.com/mohitvirli',
+    url: 'https://github.com/ChimkenKatsu',
   },
   {
     name: 'Spotify',
     hoverText: 'Curated playlists',
     icon: 'icons/spotify.svg',
-    url: 'https://open.spotify.com/user/21hr4w2hzp4ceidewwsb4bxoy',
+    url: 'https://open.spotify.com/user/31btroobucy3tw5n332yksvhgfca',
   },
   {
     name: 'Instagram',
-    hoverText: '@clevirli',
+    hoverText: '@tristanparajes',
     icon: 'icons/instagram.svg',
-    url: 'https://www.instagram.com/clevirli/',
+    url: 'https://www.instagram.com/tristanparajes/',
   },
   {
-    name: 'Resume',
-    hoverText: 'Download',
-    icon: 'icons/file.svg',
-    url: './Mohit Virli - Resume.pdf',
-  }
+  name: 'Resume',
+  hoverText: 'Download',
+  icon: 'icons/file.svg',
+  url: '/Resume.pdf',
+}
 ];
